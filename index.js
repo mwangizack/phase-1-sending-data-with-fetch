00e483f1c0627732c61139400bcb4622fd685f7d
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-13 00:07:49 +0300

    Define a function subbmitData that posts users input to the db

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return (fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        const div = document.createElement('div')
+        div.textContent = data.id
+        document.body.appendChild(div)
+    })
+    .catch(error => {
+        const p = document.createElement('p')
+        p.textContent = error.message
+        document.body.appendChild(p)
+    }))
+}
